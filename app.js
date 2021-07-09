d462dd97ea66aa911330e47bbe3d64f2043e653a
Lucia <>
2021-07-09 20:40:05 +0200

    changes to app.js

--- app.js
+++ app.js
@@ -10,9 +10,51 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 mongoose.connect("mongodb://localhost:27017/wikiDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
+
+const articleSchema = {
+	title: String,
+	content: String,
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+app
+	.route("/articles")
+	.get(function (req, res) {
+		Article.find(function (err, foundArticles) {
+			if (!err) {
+				res.send(foundArticles);
+			} else {
+				res.send(err);
+			}
+		});
+	})
+	.post(function (req, res) {
+		const newArticle = new Article({
+			title: req.body.title,
+			content: req.body.content,
+		});
+
+		newArticle.save(err);
+		if (!err) {
+			res.send("succesfully adde a new article");
+		} else {
+			res.send(err);
+		}
+	})
+	.delete(function (req, res) {
+		Article.deleteMany(function (err) {
+			if (!err) {
+				res.send("Successfully deleted all articles.");
+			} else {
+				res.send(err);
+			}
+		});
+	});
+
 app.listen(3000, function () {
-  console.log("Server started on port 3000");
+	console.log("Server started on port 3000");
 });
