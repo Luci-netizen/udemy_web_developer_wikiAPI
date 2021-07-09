29697e3031b07dedc82ec5f89638e61e29f39add
Lucia <>
2021-07-09 21:49:53 +0200

    changes to app.js

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
+// request targeting all articles
 app
 	.route("/articles")
 	.get(function (req, res) {
@@ -55,6 +56,55 @@ app
 		});
 	});
 
+// Request targeting specific article
+app
+	.route("/articles/:articleTitle")
+	.get(function (req, res) {
+		Article.findOne(
+			{ title: req.params.articleTitle },
+			function (err, foundArticle) {
+				if (foundArticle) {
+					res.send(foundArticle);
+				} else {
+					res.send("No articles matching that title was found.");
+				}
+			}
+		);
+	})
+	.put(function (req, res) {
+		Article.update(
+			{ title: req.params.articleTitle },
+			{ title: req.body.title, content: req.body.content },
+			{ overwrite: true },
+			function (err) {
+				if (!err) {
+					res.send("Succesfully updated an article.");
+				}
+			}
+		);
+	})
+	.patch(function (req, res) {
+		Article.update(
+			{ title: req.params.articleTitle },
+			{ $set: req.body },
+			function (err) {
+				if (!err) {
+					res.send("Successfully updated article");
+				} else {
+					res.send(err);
+				}
+			}
+		);
+	})
+	.delete(function (req, res) {
+		Article.deleteOne({ title: req.params.articleTitle }, function (err) {
+			if (!err) {
+				res.send("Successfully deleted the corresponding article");
+			} else {
+				res.send(err);
+			}
+		});
+	});
 app.listen(3000, function () {
 	console.log("Server started on port 3000");
 });
